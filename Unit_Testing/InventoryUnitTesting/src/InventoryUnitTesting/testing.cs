a06c06dfdc91a514652ac21220716aa4c30a8e70
Niharika Dubey <niharika@promact.com>
2017-02-20 17:48:10 +0530

    unit test assignment uploaded

---
+++ Unit_Testing/InventoryUnitTesting/src/InventoryUnitTesting/testing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+
+namespace InventoryUnitTesting
+{
+    public class Testing
+    {
+        private readonly IList<Product> _productList;
+        private readonly IList<Cart> _cartList;
+        private readonly IList<Inventory> _inventoryList;
+
+        #region Constructor
+        public Testing()
+        {
+            
+        }
+        #endregion
+    }
+}
